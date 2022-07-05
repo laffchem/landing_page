ad982246ef30d7a5cc9fb923b04e13db68dcfe96
John Laffey <laff@pop-os.localdomain>
2022-07-05 18:53:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Animate all the card elements
+$(".container").delay(100).animate({"opacity": "1"}, 1000);
+$(".image-box").delay(100).animate({"opacity": "1"}, 1000);
+$(".avatar").delay(100).animate({"opacity": "1"}, 1000);
+$(".name").delay(100).animate({"opacity": "1"}, 1000);
+$(".icons").delay(100).animate({"opacity": "1"}, 1000);
+$(".clickables").delay(100).animate({"opacity": "1"}, 1000);
+$(".rectangle").delay(100).animate({"opacity": "1"}, 1000);
+
